0e9327cb59aff410148b67830d2b22cf4f695c61
vovanezha <vova19tup@gmail.com>
2021-05-21 00:03:26 +0300

    add stylelint

---
+++ client/stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-recommended",
+    "stylelint-config-recess-order"
+  ],
+  "plugins": [
+    "stylelint-order"
+  ],
+  "rules": {
+    "selector-pseudo-class-no-unknown": null,
+  }
+}
